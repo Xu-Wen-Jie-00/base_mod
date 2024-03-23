d2c208ccd69e53406983ab9eaf208d401f31216f
Xu-Wen-Jie-00 <m19947862731@163.com>
2024-03-23 20:29:18 +0800

    向物品栏中添加物品

---
+++ src/main/java/com/xuwenjie/item/ModItems.java
@@ -0,0 +1,30 @@
+package com.xuwenjie.item;
+
+import com.xuwenjie.TemplateMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+/**
+ * @Description []
+ * @Author: 徐文杰
+ * @Date: 2024/3/23  19:29
+ */
+public class ModItems {
+    public static final Item ICE_ETHER = registerItems("ice_ether",new Item(new FabricItemSettings()));
+    private static void addItemsToItemGroups(FabricItemGroupEntries fabricItemGroupEntries){
+        fabricItemGroupEntries.add(ICE_ETHER);
+    }
+    private static Item registerItems(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier(TemplateMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToItemGroups);
+    }
+}
