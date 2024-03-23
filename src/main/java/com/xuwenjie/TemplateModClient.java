5ff0bee3e602e53aefceb17c493ed444f2df4ade
Xu-Wen-Jie-00 <m19947862731@163.com>
2024-03-23 18:51:09 +0800

    Initial commit

---
+++ src/main/java/com/xuwenjie/TemplateModClient.java
@@ -0,0 +1,15 @@
+package com.xuwenjie;
+
+import net.fabricmc.api.ClientModInitializer;
+
+/**
+ * @Description [客户端入口]
+ * @Author: 徐文杰
+ * @Date: 2024/3/23  17:58
+ */
+public class TemplateModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
